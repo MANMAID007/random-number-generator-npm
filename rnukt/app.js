9b62954946cf85fdd8ae6d1a47a0916317401670
MANMAID007 <youkti.smith@gmail.com>
2021-09-30 23:54:22 +0530

    2 new functions added

--- rnukt/app.js
+++ rnukt/app.js
@@ -1,3 +1,8 @@
 /** @format */
+// const randomGen = require("./src/RandomGen");
+
+// console.log(randomGen.random());
+// console.log(randomGen.randomN(6));
+// console.log(randomGen.randomMN(5, 10));
 
 module.exports = require("./src/RandomGen");
--- rnukt/src/RandomGen.js
+++ rnukt/src/RandomGen.js
@@ -1,5 +1,21 @@
 /** @format */
 
+// generate a random number between 0 and 1
 exports.random = function () {
     return Math.random();
+};
+
+// generate a random number between 0 an n-1
+exports.randomN = function (n) {
+    if (!Number.isInteger(n)) return `${n} is not an integer!`;
+    if (n <= 0) return `Please provide positive integer`;
+    return Math.trunc(Math.random() * n);
+};
+
+// generate a random number between m an n-1
+exports.randomMN = function (m, n) {
+    if (m >= n) return `${m} is greater or equals to ${n}, can not generate random number!`;
+    if (!Number.isInteger(m) || !Number.isInteger(n)) return `at least one of the inputed values are not an integer!`;
+    if (m <= 0) return `Please provide positive integers`;
+    return Math.trunc(Math.random() * (n - m)) + m;
 };
